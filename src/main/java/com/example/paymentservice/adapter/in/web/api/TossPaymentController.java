0682e1f6fa4507cdf4ca469e102b3cf6726d4eae
sAmH365 <npower5377@naver.com>
2025-09-16 17:57:45 +0900

    6. Toss Payments결제연동중..

---
+++ src/main/java/com/example/paymentservice/adapter/in/web/api/TossPaymentController.java
@@ -0,0 +1,19 @@
+package com.example.paymentservice.adapter.in.web.api;
+
+import com.example.paymentservice.common.WebAdapter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@WebAdapter
+@RequestMapping("/v1/toss")
+@RestController
+public class TossPaymentController {
+
+//  @PostMapping("/confirm")
+//  public Mono<String> confirm() {
+//
+//  }
+
+}
---
+++ src/main/java/com/example/paymentservice/adapter/in/web/view/PaymentController.java
@@ -0,0 +1,23 @@
+package com.example.paymentservice.adapter.in.web.view;
+
+import com.example.paymentservice.common.WebAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import reactor.core.publisher.Mono;
+
+@Controller
+@WebAdapter
+@RequestMapping("/v1/toss")
+public class PaymentController {
+
+  @GetMapping("/success")
+  public Mono<String> successPage() {
+    return Mono.just("success");
+  }
+
+  @GetMapping("/fail")
+  public Mono<String> failPage() {
+    return Mono.just("fail");
+  }
+}
